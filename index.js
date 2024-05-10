78c1b938e0701c825d31b05a9610f5105273e0ef
fteyghud <js1bane9999@hotmail.com>
2024-05-11 02:56:48 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function isPalindrome(str) {
-  return str === str.split("").reverse().join("");
-}
+const pull = (arr, ...args) => {
+  let argState = Array.isArray(args[0]) ? args[0] : args;
+  let pulled = arr.filter((v, i) => !argState.includes(v));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+};
