a4a6544a604c3a254c9cd282c1484605ed7c145d
fteyghud <js1bane9999@hotmail.com>
2024-05-12 19:19:23 +0700

    feat task

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-function canPartitionKSubsets(nums, k) {
-  const sum = nums.reduce((acc, val) => acc + val, 0);
-  if (sum % k !== 0) return false;
-  const target = sum / k;
-  nums.sort((a, b) => b - a);
-  if (nums[0] > target) return false;
-  const visited = new Array(nums.length).fill(false);
-  return backtrack(0, 0, k);
-  function backtrack(start, currentSum, groups) {
-    if (groups === 1) return true;
-    if (currentSum === target) return backtrack(0, 0, groups - 1);
-    for (let i = start; i < nums.length; i++) {
-      if (!visited[i] && currentSum + nums[i] <= target) {
-        visited[i] = true;
-        if (backtrack(i + 1, currentSum + nums[i], groups)) return true;
-        visited[i] = false;
-      }
+function numDecodings(s) {
+  const dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] === "0" ? 0 : 1;
+  for (let i = 2; i <= s.length; i++) {
+    const oneDigit = parseInt(s.substring(i - 1, i));
+    const twoDigits = parseInt(s.substring(i - 2, i));
+    if (oneDigit >= 1) {
+      dp[i] += dp[i - 1];
     }
-    return false;
+    if (twoDigits >= 10 && twoDigits <= 26) {
+      dp[i] += dp[i - 2];
+    }
   }
+  return dp[s.length];
 }
